180d38f3f666e364078506d9278a4af0fcbbd631
agentff6600 <agent.ff6600@gmail.com>
2016-04-25 20:29:37 +0200

    removed led pin

--- examples/RGB_VLC_TX/RGB_VLC_TX.ino
+++ examples/RGB_VLC_TX/RGB_VLC_TX.ino
@@ -42,11 +42,10 @@
 uint16_t transmit_data = 0;              // here we will store your message to send. The message is just a number.
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, moo);
-  
+ 
   man.workAround1MhzTinyCore();          //add this in order for transmitter to work with 1Mhz Attiny85/84
   
+}
 
 void loop() {                            // the follow programm will be repeated to infinity and beyond.
 
